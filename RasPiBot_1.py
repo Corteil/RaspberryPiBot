bf76c70ef42b4d01d44092068fdaa7bee50fccd0
Corteil <brian.corteil@gmail.com>
2014-07-30 23:45:33 +0100

    Use sleep(), instead of while loops
    use  sleep(), instead of while loop.
    
    To Do list.
    program big red button to start program!

--- RasPiBot_1.py
+++ RasPiBot_1.py
@@ -1,7 +1,7 @@
 # basic program for the Raspberry Pi Robot
 #
 # thrown together by Brian Corteil, a 45 year old script kiddie for the Raspberry Pi
-# This is free software, enjoy!!!! if you improve it please share it
+# This is free software, enjoy!!!! if you improve it, please share it
 # 
 # the robot uses the Pibrella library for Pimoroni, because it uses the Pibrella for control and sensing 
 
@@ -12,47 +12,32 @@ import pibrella
 
 # import time functions
 
-import Date
+from time import sleep
 
 # functions
 
 # move forward for 'n' seconds
 
 def forward(seconds):
-    timeStart = date.time()
-    flag = True
-    while (flag):
-        timeNow = date.time()
-        if timeNow <= (timeStart + seconds): 
-            pibrella.output.E.on()
-            pibrella.output.F.on()
-        else:
-            pibrella.output.E.off()
-            pibrella.output.F.off()
-            flag = Flase
+    
+    pibrella.output.E.on()
+    pibrella.output.F.on()
+    sleep(seconds)
+    pibrella.output.E.off()
+    pibrella.output.F.off()
             
 def left(seconds):
-    timeStart = date.time()
-    flag = True
-    while (flag):
-        timeNow = date.time()
-        if timeNow <= (timeStart + seconds):
-            pibrella.output.E.on()
-        else:
-            pibrella.output.E.off()
-            flag = Flase
-
+    
+    pibrella.output.E.on()
+    sleep(seconds)
+    pibrella.output.E.off()
+    
 def right(seconds):
-    timeStart = date.time()
-    flag = True
-    while (flag): 
-        timeNow = date.time()
-        if timeNow <= (timeStart + seconds):
-            pibrella.output.F.on()
-        else:
-            pibrella.output.F.off()
-            flag = Flase
-
+    
+    pibrella.output.F.on()
+    sleep(seconds)
+    pibrella.output.F.off()
+    
 # main program
 
 # move in a square.
